b073a09511a6eeb2e8486bb21540aaaf48c257a9
Florian-Godeau <flgodeau@gmail.com>
2023-10-18 20:49:29 +0200

    suppression fichier purgecss

--- assets/purgecss.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    content: ["../index.html", "./bootstrap/bootstrap.bundle.min.js", "./maugallery.min.js", "./script.min.js", "./style.min.css"], // Fichiers à analyser
-    css: ["./bootstrap/bootstrap.min.css"], // Votre fichier CSS à optimiser
-  };
